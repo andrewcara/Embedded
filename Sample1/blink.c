2ee8bb0ab462848c00af91f2fe393b08ea895b6d
Andrew <acarava3@uwo.ca>
2023-09-26 07:15:41 -0400

    First Functional Makefile and C script

--- Sample1/blink.c
+++ Sample1/blink.c
@@ -1,15 +1,28 @@
-#define F_CPU 16000000UL
-#define LED_PIN 0
-
 #include <avr/io.h>
 #include <util/delay.h>
 
-void main(void)
-{
-	DDRB |= (1 << LED_PIN); // set pin as output
-    while (1) 
-    {
-		PORTB ^= (1 << LED_PIN); // toggles  pin		
-		_delay_ms(500000); // busy wait, 500ms
+#define LED_PIN 0 // Define the digital pin for the LED
+#define LED_PIN2 4
+int main(void) {
+    // Set the LED pin as an output
+    DDRD |= (1 << LED_PIN);
+    
+    DDRB |= (1 << LED_PIN);
+
+    while (1) {
+        // Turn the LED on
+        PORTD |= (1 << LED_PIN);
+        PORTB |= (1 << LED_PIN2);
+        _delay_ms(1000); // Wait for 1 second (1000 milliseconds)
+
+        // Turn the LED off
+        PORTD &= ~(1 << LED_PIN);
+        _delay_ms(1000); // Wait for 1 second
+        // Turn the LED off
+        PORTB &= ~(1 << LED_PIN2);
+        _delay_ms(2000); // Wait for 1 second
     }
+
+    return 0;
 }
+
